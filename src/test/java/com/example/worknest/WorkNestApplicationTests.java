1d2561bba402284ceb93adab119cd60ee16d3443
BiOksana <biokissme@gmail.com>
2025-07-03 16:38:10 +0200

    initial commit

---
+++ src/test/java/com/example/worknest/WorkNestApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.worknest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootTest
+//@EnableJpaRepositories
+class WorkNestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
